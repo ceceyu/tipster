d9bace985ec40da1b17f2c9b1aab8dd79cc3f5a6
ceceyu <ceceyu@gmail.com>
2015-04-13 03:29:54 -0700

    Commit of code
    Working tip calculator with some minor adjustments to type. Some custom
    images. Keyboard shows up automatically.

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        // Makes sure the keyboard pops up upon load
+        billField.becomeFirstResponder()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +32,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.15, 0.2, 0.22, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
 }
 
